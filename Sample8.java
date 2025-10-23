44b7bf7ef1d48ed7ebe906226aba55e0034230a5
Jim9025 <jim0988845382@gmail.com>
2025-10-23 18:22:37 +0800

    Create Sample8.java

---
+++ Sample8.java
@@ -0,0 +1,12 @@
+public class Sample8 {
+    public static void main(String[] args) {
+        int num = 3;   // 宣告整數型變數 num 並給初始值 3
+
+        System.out.println("變數num的值是" + num);
+
+        num = 5;       // 將變數 num 的內容更新為 5
+
+        System.out.println("更新變數num的值");
+        System.out.println("變數num更新後的值是" + num);
+    }
+}
