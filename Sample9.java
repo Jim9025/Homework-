667a59f0e850c551dba6315a3057a72fac1c59e1
Jim9025 <jim0988845382@gmail.com>
2025-10-23 18:25:03 +0800

    Create Sample9.java

---
+++ Sample9.java
@@ -0,0 +1,12 @@
+public class Sample9 {
+    public static void main(String[] args) {
+        int num1, num2;   // 宣告兩個整數變數 num1、num2
+
+        num1 = 3;         // 將數值 3 指定給 num1
+        System.out.println("變數num1的值是：" + num1);
+
+        num2 = num1;      // 將 num1 的值指定給 num2
+        System.out.println("將變數num1指定到變數num2之中");
+        System.out.println("變數num2的值是：" + num2);
+    }
+}
