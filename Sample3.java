0a0e9b93e78206e0cdb7d063fd8f2970e881831a
Jim9025 <jim0988845382@gmail.com>
2025-10-23 18:14:48 +0800

    Create Sample3.java

---
+++ Sample3.java
@@ -0,0 +1,7 @@
+public class Sample3 {
+    public static void main(String[] args) {
+        System.out.println('A');               // 輸出字元常數
+        System.out.println("歡迎使用Java!");   // 輸出字串常數
+        System.out.println(123);               // 輸出整數常數
+    }
+}
