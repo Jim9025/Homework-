336ed454f207f1c525f832a2e56fb784e3f649c5
Jim9025 <jim0988845382@gmail.com>
2025-10-23 18:20:32 +0800

    Create Sample7.java

---
+++ Sample7.java
@@ -0,0 +1,7 @@
+public class Sample7 {
+    public static void main(String[] args) {
+        int num;                       // 宣告變數 num（整數型）
+        num = 3;                       // 將數值 3 指定給變數 num
+        System.out.println("變數num的值是" + num); // 輸出變數 num 的內容
+    }
+}
